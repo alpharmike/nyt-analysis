7a3a7e6759875acabc239ad64faea411ca99c61a
Erfan Moeini <ec.moeini01@gmail.com>
2022-03-18 15:29:43 +0330

    Add filtering and final aggregation to transformer

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -7,6 +7,154 @@ from kafka import KafkaProducer
 logger = logging.getLogger(__name__)
 
 
+DATA = {
+    "abstract": "The Crimson Tide will face the Georgia Bulldogs on Jan. 10 for the national championship.",
+    "web_url": "https://www.nytimes.com/2021/12/31/sports/ncaafootball/alabama-cincinnati-cotton-bowl-score.html",
+    "snippet": "The Crimson Tide will face the Georgia Bulldogs on Jan. 10 for the national championship.",
+    "lead_paragraph": "ARLINGTON, Texas — This was the moment the Cincinnati Bearcats had pined for all season. The roster is chock-full of local kids, overlooked by Ohio State in high school and largely underestimated now by the football elite, who put their place in the College Football Playoff down to a matter of necessity: The sport needed a fourth team in the field.",
+    "print_section": "B",
+    "print_page": "6",
+    "source": "The New York Times",
+    "multimedia": [
+        {
+            "rank": 0,
+            "subtype": "xlarge",
+            "caption": None,
+            "credit": None,
+            "type": "image",
+            "url": "images/2021/12/31/sports/31cfb-cotton01/merlin_199794252_d5688c82-37da-401c-a721-d300940aebc6-articleLarge.jpg",
+            "height": 400,
+            "width": 600,
+            "subType": "xlarge",
+            "crop_name": "articleLarge",
+            "legacy": {
+                "xlarge": "images/2021/12/31/sports/31cfb-cotton01/merlin_199794252_d5688c82-37da-401c-a721-d300940aebc6-articleLarge.jpg",
+                "xlargewidth": 600,
+                "xlargeheight": 400
+            }
+        },
+        {
+            "rank": 0,
+            "subtype": "jumbo",
+            "caption": None,
+            "credit": None,
+            "type": "image",
+            "url": "images/2021/12/31/sports/31cfb-cotton01/merlin_199794252_d5688c82-37da-401c-a721-d300940aebc6-jumbo.jpg",
+            "height": 683,
+            "width": 1024,
+            "subType": "jumbo",
+            "crop_name": "jumbo",
+            "legacy": {}
+        },
+        {
+            "rank": 0,
+            "subtype": "superJumbo",
+            "caption": None,
+            "credit": None,
+            "type": "image",
+            "url": "images/2021/12/31/sports/31cfb-cotton01/merlin_199794252_d5688c82-37da-401c-a721-d300940aebc6-superJumbo.jpg",
+            "height": 1365,
+            "width": 2048,
+            "subType": "superJumbo",
+            "crop_name": "superJumbo",
+            "legacy": {}
+        },
+        {
+            "rank": 0,
+            "subtype": "thumbnail",
+            "caption": None,
+            "credit": None,
+            "type": "image",
+            "url": "images/2021/12/31/sports/31cfb-cotton01/31cfb-cotton01-thumbStandard.jpg",
+            "height": 75,
+            "width": 75,
+            "subType": "thumbnail",
+            "crop_name": "thumbStandard",
+            "legacy": {
+                "thumbnail": "images/2021/12/31/sports/31cfb-cotton01/31cfb-cotton01-thumbStandard.jpg",
+                "thumbnailwidth": 75,
+                "thumbnailheight": 75
+            }
+        },
+        {
+            "rank": 0,
+            "subtype": "thumbLarge",
+            "caption": None,
+            "credit": None,
+            "type": "image",
+            "url": "images/2021/12/31/sports/31cfb-cotton01/31cfb-cotton01-thumbLarge.jpg",
+            "height": 150,
+            "width": 150,
+            "subType": "thumbLarge",
+            "crop_name": "thumbLarge",
+            "legacy": {}
+        }
+    ],
+    "headline": {
+        "main": "Alabama Rolls Past Cincinnati, 27-6, in College Football Playoff Semifinal",
+        "kicker": "Alabama 27, Cincinnati 6",
+        "content_kicker": None,
+        "print_headline": "The Tide Rolls On",
+        "name": None,
+        "seo": None,
+        "sub": None
+    },
+    "keywords": [
+        {
+            "name": "subject",
+            "value": "Football (College)",
+            "rank": 1,
+            "major": "N"
+        },
+        {
+            "name": "organizations",
+            "value": "University of Alabama",
+            "rank": 2,
+            "major": "N"
+        },
+        {
+            "name": "organizations",
+            "value": "University of Cincinnati",
+            "rank": 3,
+            "major": "N"
+        },
+        {
+            "name": "subject",
+            "value": "Cotton Bowl (Football Game)",
+            "rank": 4,
+            "major": "N"
+        },
+        {
+            "name": "subject",
+            "value": "College Football Playoff National Championship",
+            "rank": 5,
+            "major": "N"
+        }
+    ],
+    "pub_date": "2022-01-01T00:01:35+0000",
+    "document_type": "article",
+    "news_desk": "Sports",
+    "section_name": "Sports",
+    "subsection_name": "College Football",
+    "byline": {
+        "original": "By Billy Witz",
+        "person": [
+            {
+                "firstname": "Billy",
+                "lastname": "Witz",
+                "role": "reported",
+                "organization": "",
+                "rank": 1
+            }
+        ],
+    },
+    "type_of_material": "News",
+    "_id": "nyt://article/50aa9e4f-5d58-5577-9492-99f66707b3ce",
+    "word_count": 1245,
+    "uri": "nyt://article/50aa9e4f-5d58-5577-9492-99f66707b3ce"
+}
+
+
 class ScrapeHandler:
     def __init__(self):
         self._producer: KafkaProducer = None
@@ -43,11 +191,11 @@ class ScrapeHandler:
         month = start_month
         for year in range(start_year, end_year + 1):
             while month <= 12 and (year < end_year or month <= end_month):
-                data = self._fetch_news_by_date(month, year)
+                # data = self._fetch_news_by_date(month, year)
                 try:
-                    data = data["response"]["docs"]
-                    sliced_data = data[:2]
-                    self._producer.send(default_config["KAFKA"]["ARCHIVE_TOPIC"], sliced_data)
+                    # data = data["response"]["docs"]
+                    # data = data[0]
+                    self._producer.send(default_config["KAFKA"]["ARCHIVE_TOPIC"], DATA)
                 except Exception as error:
                     logger.error(f"Could not parse NYT API response - ${str(error)}")
                 month += 1
--- src/streamer/transformer.py
+++ src/streamer/transformer.py
@@ -1,10 +1,12 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
-from pyspark.sql.types import StructType, StructField, IntegerType, LongType, StringType, MapType, ArrayType, DataType
-from pyspark.sql.functions import from_json, col, explode
+from pyspark.sql.types import StructType, StructField, IntegerType, LongType, StringType, MapType, ArrayType, DateType, \
+    TimestampType
+from pyspark.sql.functions import from_json, col, explode, lower, concat, lit, collect_list, unix_timestamp
 from src.utils.config_parser import default_config
 
 schema = StructType([
+    StructField("_id", StringType(), True),
     StructField("abstract", StringType(), True),
     StructField("web_url", StringType(), True),
     StructField("lead_paragraph", StringType(), True),
@@ -26,12 +28,12 @@ schema = StructType([
         ),
         True
     ),
-    StructField("pub_date", DataType(), True),
+    StructField("pub_date", DateType(), True),
     StructField("section_name", StringType(), True),
     StructField("subsection_name", StringType(), True),
     StructField("type_of_material", StringType(), True),
 ])
-schema = ArrayType(schema, False)
+
 # Create a SparkSession
 spark = SparkSession.builder.appName("NYT Streamer").getOrCreate()
 
@@ -44,12 +46,24 @@ df = spark \
     .load()
 
 parsed_data_df = df.selectExpr("CAST(value AS STRING)")
-converted_df = parsed_data_df.select(explode(from_json(col("value"), schema).alias("data"))).select("col.*")
-converted_df.printSchema()
-converted_df.writeStream \
+news_df = parsed_data_df.select(from_json(col("value"), schema).alias("data")).select("data.*")
+
+concat_df = news_df.select("_id", "abstract", "lead_paragraph", col("headline.main").alias("head_main"), explode("keywords").alias("keywords"), "pub_date") \
+    .select("_id", "abstract", "lead_paragraph", "head_main", col("keywords.value").alias("keyw_value"), "pub_date") \
+    .withColumn("concat_cols", concat(col('abstract'), lit(" "), col('lead_paragraph'), lit(" "), col('head_main'), lit(" "), col('keyw_value'))) \
+
+filtered_df = concat_df.filter(
+    (lower(concat_df.concat_cols).contains("crimson")) | (lower(concat_df.concat_cols).contains("russia")) \
+    | (lower(concat_df.concat_cols).contains("nato")) | (lower(concat_df.concat_cols).contains("war"))
+)
+
+grouped_news = filtered_df.groupBy("_id", "abstract", "lead_paragraph", "head_main", "pub_date").agg(collect_list("keyw_value").alias("keywords"))
+
+grouped_news.printSchema()
+grouped_news.writeStream \
       .format("console") \
       .option("numRows", 20) \
-      .outputMode("append") \
+      .outputMode("complete") \
       .start() \
       .awaitTermination()
 
